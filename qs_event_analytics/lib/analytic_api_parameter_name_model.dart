366a6b2bb6d4c57702e6edc30e7935feda70ced2
Song <791589545@qq.com>
2026-09-05 13:53:45 +0800

    2026.09.05
    1.1.3

--- qs_event_analytics/lib/analytic_api_parameter_name_model.dart
+++ qs_event_analytics/lib/analytic_api_parameter_name_model.dart
@@ -16,18 +16,32 @@ class AnalyticApiParameterNameModel {
     required this.env,
   });
 
+  // 会话ID
   final String sessionId;
+  // 用户ID
   final String uuid;
+  // 事件编码
   final String eventCode;
+  // 事件名称
   final String eventName;
+  //  事件类型
   final String eventType;
+  // 事件时间
   final String eventTime;
+  // 用户IP
   final String userIp;
+  // 国家代码
   final String countryCode;
+  // 城市代码
   final String cityCode;
+  // 系统版本
   final String systemVersion;
+  // 应用版本
   final String appVersion;
+  // 所属页面
   final String attrPage;
+  // 事件内容（额外参数）
   final String eventContent;
+  // 环境
   final String env;
 }
